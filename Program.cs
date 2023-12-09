9c4a0b386acc33a0ef7b03ce0c5d98b2e641db68
carloshenriquearruda <carloshenriquearruda82@gmail.com>
2023-12-09 16:46:03 -0300

    calculadora

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
-﻿using C__Start.Models;
+﻿// using C__Start.Models;
 
-Pessoa pessoa1 = new Pessoa();
+// Pessoa pessoa1 = new Pessoa();
 
-pessoa1.Nome= "Carlos";
-pessoa1.Idade= 23;
-pessoa1.Apresentar();
+// pessoa1.Nome= "Carlos";
+// pessoa1.Idade= 23;
+// pessoa1.Apresentar();
+
+using C__Start.Models;
+
+Calculadora calc = new Calculadora();
+
+calc.Somar(5,5);
+calc.Subtrair(5,5);
+calc.Multiplicar(5,5);
+calc.Dividir(5,5);
