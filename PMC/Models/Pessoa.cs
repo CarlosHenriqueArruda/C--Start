517e53741fac37ac7b912cb8199e2f5e8cbc7c1c
carloshenriquearruda <carloshenriquearruda82@gmail.com>
2023-12-10 12:04:04 -0300

    classe pessoa

---
+++ PMC/Models/Pessoa.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PMC.Models
+{
+    public class Pessoa
+    {
+        private string _nome;
+        private string _sobrenome;
+        private int _idade;
+        public string Nome
+        { 
+            get => _nome.ToUpper();
+            
+            set
+            {
+                if(value == "")
+                {
+                    throw new ArgumentException("O nome não pode ser vazio.");
+                }
+                _nome = value;
+            }
+        }
+        public string Sobrenome 
+        { 
+            get => _sobrenome.ToUpper();
+            
+            set
+            {
+                if(value == "")
+                {
+                    throw new ArgumentException("O sobrenome não pode ser vazio.");
+                }
+                _sobrenome = value;
+            }
+        }
+        public string NomeCompleto =>$"{Nome} {Sobrenome}";
+        public int Idade
+        { 
+            get => _idade;
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("A idade não pode ser menor que: 0");
+                }
+            }
+        }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Nome: {NomeCompleto} - Idade: {Idade}");
+        }
+    }
+}
---
+++ PMC/Program.cs
@@ -0,0 +1,9 @@
+﻿using PMC.Models;
+
+
+
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "Carlos";
+p1.Idade = 23;
+p1.Apresentar();
